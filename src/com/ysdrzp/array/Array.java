b2b258b1007ab9b85700942e384089cc579db1e9
zhaizhipeng <hitpeng1230>
2020-04-15 20:10:37 +0800

    优化

--- src/com/ysdrzp/array/Array.java
+++ src/com/ysdrzp/array/Array.java
@@ -13,149 +13,12 @@ public class Array<E> {
     private E[] data;
 
     public Array(int capacity){
-        data = (E[]) new Objects[capacity];
+        data = (E[]) new Object[capacity];
         size = 0;
     }
 
     public Array(){
-        this.data = (E[]) new Objects[10];
-    }
-
-    /**
-     * 指定索引位置插入元素
-     * @param index
-     * @param element
-     */
-    public void addElement(int index, E element){
-        // 保证数组连续性
-        if (index < 0 || index > size){
-            throw new IllegalArgumentException("参数非法");
-        }
-
-        // 触发扩容
-        if (size == data.length){
-            resize();
-        }
-
-        for (int i = size; i > index; i--){
-            data[i] = data[i-1];
-        }
-        data[index] = element;
-        size++;
-    }
-
-    /**
-     * 末尾添加元素
-     * @param element
-     * @return
-     */
-    public boolean addLast(E element){
-        addElement(size, element);
-        return true;
-    }
-
-    /**
-     * 首位添加元素
-     * @param element
-     * @return
-     */
-    public boolean addFirst(E element){
-        addElement(0, element);
-        return true;
-    }
-
-    /**
-     * 删除指定索引位置的元素
-     * @param index
-     * @return 返回删除元素
-     */
-    public E remove(int index){
-        if (index < 0 || index > size){
-            throw new IllegalArgumentException("参数非法");
-        }
-
-        if (size == 0){
-            throw new IllegalArgumentException("Array is Empty");
-        }
-
-        E element = data[index];
-
-        for (int i = index; i < size-1; i++){
-            data[i] = data[i + 1];
-        }
-        size--;
-        return element;
-    }
-
-    /**
-     * 删除指定元素
-     * @param element
-     * @return
-     */
-    public boolean removeElement(E element){
-        int index = find(element);
-        if (index == -1){
-            return false;
-        }
-        remove(index);
-        return true;
-    }
-
-    /**
-     * 删除末尾元素
-     * @return 返回删除元素
-     */
-    public E removeLast(){
-        this.remove(size-1);
-        return data[size-1];
-    }
-
-    /**
-     * 删除首位元素
-     * @return 返回删除元素
-     */
-    public E removeFirst(){
-        this.remove(0);
-        return data[0];
-    }
-
-    /**
-     * 查找指定元素
-     * @param element
-     * @return 返回元素索引位置
-     */
-    public int find(E element){
-        for (int i = 0; i < size; i++){
-            if (data[i] == element){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    /**
-     * 修改指定索引位置元素
-     * @param index
-     * @param element
-     * @return
-     */
-    public boolean setElement(int index, E element){
-        if (index < 0 || index > size){
-            throw new IllegalArgumentException("参数非法");
-        }
-        data[index] = element;
-        return true;
-    }
-
-    /**
-     * 2倍扩容
-     */
-    public void resize(){
-        E[] newData = (E[]) new Objects[data.length * 2];
-        for (int i = 0; i < size; i++){
-            newData[i] = data[i];
-        }
-        data = newData;
+        this(10);
     }
 
     /**
@@ -174,10 +37,178 @@ public class Array<E> {
         return size == 0;
     }
 
+    /**
+     * 获取数组的容量
+     * @return
+     */
+    public int getCapacity(){
+        return data.length;
+    }
+
+    /**
+     * 指定索引位置插入元素
+     * @param index
+     * @param element
+     */
+    public void addElement(int index, E element){
+        // 保证数组连续性
+        if (index < 0 || index > size){
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+        }
+
+        // 扩容
+        if (size == data.length){
+            resize(data.length * 2);
+        }
+
+        for (int i = size; i > index; i--){
+            data[i] = data[i-1];
+        }
+        data[index] = element;
+        size++;
+    }
+
+    /**
+     * 末尾添加元素
+     * @param element
+     * @return
+     */
+    public void addLast(E element){
+        addElement(size, element);
+    }
+
+    /**
+     * 首位添加元素
+     * @param element
+     * @return
+     */
+    public void addFirst(E element){
+        addElement(0, element);
+    }
+
+    /**
+     * 获取index索引位置的元素
+     * @param index
+     * @return
+     */
+    public E get(int index){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Index is illegal.");
+        return data[index];
+    }
+
+    /**
+     * 修改指定索引位置元素
+     * @param index
+     * @param element
+     * @return
+     */
+    public void set(int index, E element){
+        if (index < 0 || index > size){
+            throw new IllegalArgumentException("Set failed. Index is illegal.");
+        }
+        data[index] = element;
+    }
+
+    /**
+     * 查找数组中元素element所在的索引，如果不存在元素element，则返回-1
+     * @param element
+     * @return 返回元素索引位置
+     */
+    public int find(E element){
+        for (int i = 0; i < size; i++){
+            if (data[i] == element){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找数组中是否有元素element
+     * @param element
+     * @return
+     */
+    public boolean contains(E element){
+        int index = find(element);
+        if (index >= 0){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 删除指定索引位置的元素
+     * @param index
+     * @return 返回删除元素
+     */
+    public E remove(int index){
+        if (index < 0 || index > size){
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
+        }
+
+        if (size == 0){
+            throw new IllegalArgumentException("Array is Empty");
+        }
+
+        E element = data[index];
+
+        for (int i = index; i < size-1; i++){
+            data[i] = data[i + 1];
+        }
+        size--;
+        data[size] = null;
+
+        // 缩容，防止复杂度震荡
+        if(size == data.length / 4 && data.length / 2 != 0)
+            resize(data.length / 2);
+        return element;
+    }
+
+    /**
+     * 删除末尾元素
+     * @return 返回删除元素
+     */
+    public E removeLast(){
+        return remove(size-1);
+    }
+
+    /**
+     * 删除首位元素
+     * @return 返回删除元素
+     */
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    /**
+     * 删除指定元素
+     * @param element
+     * @return
+     */
+    public void removeElement(E element){
+        int index = find(element);
+        if (index >= 0){
+            remove(index);
+        }
+    }
+
+    /**
+     * 将数组空间的容量变成 newCapacity 大小
+     */
+    public void resize(int newCapacity){
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++){
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
     @Override
     public String toString(){
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("[" + String.format("size = %d,capacity = %d\n", size, data.length));
+        stringBuilder.append(String.format("size = %d,capacity = %d\n", size, data.length));
+        stringBuilder.append("[");
         for (int i = 0; i < size; i++){
             stringBuilder.append(data[i]);
             if (i != size - 1){
@@ -189,39 +220,30 @@ public class Array<E> {
     }
 
     public static void main(String[] args) {
+        Array<Integer> arr = new Array<>();
+        for(int i = 0 ; i < 10 ; i ++)
+            arr.addLast(i);
+        System.out.println(arr);
 
-        Array<Integer> array = new Array<>(10);
-        System.out.println(array);
+        arr.addElement(1, 100);
+        System.out.println(arr);
 
-        for (int i = 0; i < 5; i++){
-            array.addLast(i);
+        arr.addFirst(-1);
+        System.out.println(arr);
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
+
+        arr.removeFirst();
+        System.out.println(arr);
+
+        for(int i = 0 ; i < 4 ; i ++){
+            arr.removeFirst();
+            System.out.println(arr);
         }
-        System.out.println(array);
-
-        array.addElement(0,100);
-        System.out.println(array);
-
-        array.addFirst(200);
-        System.out.println(array);
-
-        System.out.println(array.removeLast());
-        System.out.println(array);
-
-        System.out.println(array.removeFirst());
-        System.out.println(array);
-
-        System.out.println(array.remove(2));
-        System.out.println(array);
-
-        array.addLast(111);
-        array.addLast(222);
-        array.addLast(333);
-        array.addLast(444);
-        array.addLast(555);
-        System.out.println(array);
-        array.addLast(666);
-        array.addLast(777);
-        System.out.println(array);
     }
 
 }
