cbdb619dc6586eec7d87e7aca4782b9fc4b9d9ba
zhaizhipeng <hitpeng1230>
2020-04-19 10:41:22 +0800

    冒泡排序算法

---
+++ src/com/ysdrzp/sort/BubbleSort.java
@@ -0,0 +1,39 @@
+package com.ysdrzp.sort;
+
+/**
+ * 冒泡排序
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] a = {6,5,3,7,8,1,2,0,4,9};
+        bubbleSort(a, a.length);
+        for (int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+
+    /**
+     * 冒泡排序，a表示数组，n表示数组大小
+     * @param a
+     * @param n
+     */
+    public static void bubbleSort(int[] a, int n) {
+        if (n <= 1) return;
+
+        for (int i = 0; i < n; ++i) {
+            // 提前退出冒泡循环的标志位
+            boolean flag = false;
+            for (int j = 0; j < n - i - 1; ++j) {
+                if (a[j] > a[j+1]) { // 交换
+                    int tmp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = tmp;
+                    flag = true;  // 表示有数据交换
+                }
+            }
+            if (!flag) break;  // 没有数据交换，提前退出
+        }
+    }
+
+}
